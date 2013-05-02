d67beb8e492916dd5193b6cecddc192eb2efc9aa
austinrory <austinroryhackett@gmail.com>
2013-05-01 23:22:45 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wayne::Application.config.secret_token = '1bcf6ef5151e8e4a297eca0eb96c9707ce9094a7991faf7468fd14202f4d90b86614ef3ad7894f443bc0899bf748d1fa3fb26c0a9b9f9aeb0d00db240b9fc7c5'
